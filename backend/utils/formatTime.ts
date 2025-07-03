d4743a127ba5e27caff4a69517127d37d5908ba8
Abdullah Al Mridul <abdullah.github@zohomail.com>
2025-07-03 04:31:00 -0700

    second added in rename

--- backend/utils/formatTime.ts
+++ backend/utils/formatTime.ts
@@ -1,12 +1,22 @@
 const formatTimestamp = () => {
   const now = new Date();
+
   const date = now.toLocaleDateString("en-GB").replace(/\//g, "-");
+
   let hours = now.getHours();
   const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const milliseconds = now.getMilliseconds().toString().padStart(3, "0");
   const ampm = hours >= 12 ? "PM" : "AM";
+
   hours = hours % 12 || 12;
-  const time = `${hours.toString().padStart(2, "0")}.${minutes}${ampm}`;
+
+  const time = `${hours
+    .toString()
+    .padStart(2, "0")}.${minutes}.${seconds}.${milliseconds}${ampm}`;
+
   return `${date}_${time}`;
 };
+
 export default formatTimestamp;
 module.exports = formatTimestamp;
