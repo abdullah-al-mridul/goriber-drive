c1a46f917755526cfdc3fc1bb44f50311089a3da
Abdullah Al Mridul <abdullah.github@zohomail.com>
2025-07-03 06:52:42 -0700

    remove space from file name

--- backend/routes/file.routes.ts
+++ backend/routes/file.routes.ts
@@ -26,7 +26,7 @@ fileRouter.post("/upload", auth, (req: any, res: any) => {
       const { filename, encoding, mimeType } = info;
       console.log(`File [${name}]: filename: %j`, filename);
       const ext = path.extname(filename);
-      const base = path.basename(filename, ext);
+      const base = path.basename(filename.replace(/\s+/g, ""), ext);
       const timestamp = formatTimestamp();
       const finalName = `${base}-${timestamp}${ext}`;
       const saveTo = path.join(uploadDir, finalName);
@@ -76,6 +76,39 @@ fileRouter.get("/files", auth, (req: any, res: any) => {
       }
       console.log(files);
       res.status(200).json(files);
+    });
+  } else {
+    console.error(
+      "DATA_PATH is not set or is not a string. If data path set then make sure it is a valid folder in that directory ."
+    );
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+fileRouter.get("fileInfo/:filename", auth, (req: any, res: any) => {
+  const { filename } = req.params;
+  const uploadDir = env.DATA_PATH;
+  if (
+    typeof uploadDir === "string" &&
+    uploadDir.trim() !== "" &&
+    fs.existsSync(uploadDir) &&
+    fs.statSync(uploadDir).isDirectory()
+  ) {
+    const filePath = path.join(uploadDir, filename);
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error("Error getting file stats:", err);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+
+      res.status(200).json({
+        name: filename,
+        size: stats.size,
+        isFile: stats.isFile(),
+        isDirectory: stats.isDirectory(),
+        createdAt: stats.birthtime,
+        modifiedAt: stats.mtime,
+      });
     });
   } else {
     console.error(
